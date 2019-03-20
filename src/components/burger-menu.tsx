428346215d43bf669e47beebb944f77008f5611a
oephi <r.sickle@protonmail.com>
2019-03-20 15:37:23 +1100

    Replace burger menu with sitemetadata

--- src/components/burger-menu.tsx
+++ src/components/burger-menu.tsx
@@ -1,48 +1,24 @@
 import React from "react"
 import { slide as Menu } from "react-burger-menu"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
 
-export default () => (
-  <div>
-    <Menu right width={"45%"} noOverlay>
-      <main>
-        <ul className="bm-item-list">
-          <li>
-            <a id="home" className="menu-item" href="/">
-              Home
-            </a>
-          </li>
-          <li>
-            <a id="case-studies" className="menu-item" href="/case-studies">
-              Case Studies
-            </a>
-          </li>
-          <li>
-            <a id="careers" className="menu-item" href="/careers">
-              Careers
-            </a>
-          </li>
-          <li>
-            <a id="purpose" className="menu-item" href="/purpose">
-              Purpose
-            </a>
-          </li>
-          <li>
-            <a id="team" className="menu-item" href="/team">
-              Team
-            </a>
-          </li>
-          <li>
-            <a id="community" className="menu-item" href="/community">
-              Community
-            </a>
-          </li>
-          <li>
-            <a id="Contact" className="menu-item" href="/contact">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </main>
-    </Menu>
-  </div>
-)
+export default () => {
+  const { menulinks } = useSiteMetadata()
+  return (
+    <div>
+      <Menu right width={"45%"} noOverlay>
+        <main>
+          <ul className="bm-item-list">
+            {menulinks.map((menulink: any) => (
+              <li key={menulink.name}>
+                <a id="case-studies" className="menu-item" href={menulink.link}>
+                  {menulink.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </main>
+      </Menu>
+    </div>
+  )
+}
