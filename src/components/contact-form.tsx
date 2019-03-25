9b1c7983150460eb1313f930612bb6af41079fb0
Josh Price <josh@alembic.com.au>
2019-03-25 14:39:54 +1100

    Add a honeypot field to form

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -2,8 +2,14 @@ import React from "react"
 
 export default () => (
   <div className="form">
-    <form name="contact" action="/contact" method="POST" data-netlify="true">
+    <form
+      name="contact"
+      action="/contact"
+      netlify-honeypot="message"
+      method="POST"
+      data-netlify="true">
       <h2>Subscribe to our newsletter</h2>
+      <input type="hidden" name="message" />
       <input type="email" name="email" placeholder="your.name@example.com" />
       <button className="btn btn-primary" type="submit">
         Subscribe
