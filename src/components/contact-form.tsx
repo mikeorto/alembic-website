3aaf8aefb4ca9452aa87dbe198dd5e461dac05e2
Josh Price <josh@alembic.com.au>
2019-03-25 14:43:34 +1100

    Try form again with hidden form-name

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -2,14 +2,9 @@ import React from "react"
 
 export default () => (
   <div className="form">
-    <form
-      name="contact"
-      action="/contact"
-      netlify-honeypot="message"
-      method="POST"
-      data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true">
       <h2>Subscribe to our newsletter</h2>
-      <input type="hidden" name="message" />
+      <input type="hidden" name="form-name" value="contact" />
       <input type="email" name="email" placeholder="your.name@example.com" />
       <button className="btn btn-primary" type="submit">
         Subscribe
