d000dc836a32ad70b53482459e3aae9f633714a6
oephi <r.sickle@protonmail.com>
2019-05-09 11:39:59 +1000

    Add CTA button on homepage and why alembic page

--- src/components/service-offerings.tsx
+++ src/components/service-offerings.tsx
@@ -5,6 +5,11 @@ export default () => (
   <div id="what-we-do" className="what-we-do wrap">
     <aside className="stacked">
       <ServiceOfferingsContent />
+      <div className="phone" >
+        <a href="/contact" className="btn btn-primary">
+          Get in touch!
+        </a>
+      </div>
     </aside>
   </div>
 )
